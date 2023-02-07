3b3add37a0831aa99fbf4abafeda7cefb3795093
Tilio <tilio.bourigault@ynov.com>
2023-02-07 08:21:19 +0100

    update login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -6,7 +6,7 @@ export const Login = () => {
         <body>
             <div className='item'>
             <button type="button">Cliquez ici</button>
-                <div id='box1'></div>
+                <div id='box14'></div>
                 <div id='box6'></div>
 
 
