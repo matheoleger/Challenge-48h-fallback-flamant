e40fb6de52e0f3ed93cefc85fdb632b15d42cb6c
smathis <mathis.silotia@ynov.com>
2023-02-07 05:47:12 +0100

    create card template

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,21 +3,94 @@ import image from "../assets/img/paysage.png"
 
 import "../assets/css/Home.css"
 
+class Item {
+    name : string
+    price : number
+    desc : string
+
+    constructor(name : string, price : number, desc : string){
+        this.name = name
+        this.price = price
+        this.desc = desc
+    }
+}
+
+
 export const Home = () => {
+    const prix = "5€"
+    const table = new Item("Table",500,"Il y a 4 pied")
+
     return (
-    <body>
         
-        <div className='item'>
-            <div id='box1'>
-            <img src={image}/>
+        <body>
+            <div className="topnav">
+                <a className="active" href="#home">Home</a>
+                <a href="#about">About</a>
+
+                <div className="search-container">
+                <form action="">
+                    <input type="text" placeholder="Search.." name="search" autoComplete='off'/>
+                    <button type="submit"><i className="fa fa-search"></i></button>
+                </form>
             </div>
-            <div id='box2'></div>
-            <div id='box3'></div>
-            <div id='box4'></div>
-            <div id='box5'></div>
-            <div id='box6'></div>
-            <div id='box7'></div>
-            <div id='box8'></div>
+        </div>
+        <div id='item'>
+            <div id='box'>
+            <img src={image}/>
+            <p id='name'>{table.name}</p>
+            <p id='price'>{table.price}€</p>
+            <p id='desc'>{table.desc}</p>
+            </div>
+            
+            <div id='box'>
+            <img src={image}/>
+            <p id='name'>{table.name}</p>
+            <p id='price'>{table.price}€</p>
+            <p id='desc'>{table.desc}</p>
+            </div>
+
+            <div id='box'>
+            <img src={image}/>
+            <p id='name'>{table.name}</p>
+            <p id='price'>{table.price}€</p>
+            <p id='desc'>{table.desc}</p>
+            </div>
+
+            <div id='box'>
+            <img src={image}/>
+            <p id='name'>{table.name}</p>
+            <p id='price'>{table.price}€</p>
+            <p id='desc'>{table.desc}</p>
+            </div>
+
+            <div id='box'>
+            <img src={image}/>
+            <p id='name'>{table.name}</p>
+            <p id='price'>{table.price}€</p>
+            <p id='desc'>{table.desc}</p>
+
+            </div>
+
+            <div id='box'>
+            <img src={image}/>
+            <p id='name'>{table.name}</p>
+            <p id='price'>{table.price}€</p>
+            <p id='desc'>{table.desc}</p>
+            </div>
+
+            <div id='box'>
+            <img src={image}/>
+            <p id='name'>{table.name}</p>
+            <p id='price'>{table.price}€</p>
+            <p id='desc'>{table.desc}</p>
+            </div>
+
+            <div id='box'>
+            <img src={image}/>
+            <p id='name'>{table.name}</p>
+            <p id='price'>{table.price}€</p>
+            <p id='desc'>{table.desc}</p>
+            </div>
         </div>
     </body>
         
